bb2b894f19674a3704f86718f7edc7bef1e7c9c7
Ivan Cvitkovic <ivanc@uw.edu>
2025-06-09 16:07:57 -0700

    Fix typing issue

--- Controller/OpauthAppController.php
+++ Controller/OpauthAppController.php
@@ -94,7 +94,7 @@ class OpauthAppController extends AppController {
 				);
 				$response['validated'] = false;
 			}
-			elseif (!($this->Opauth->validate(sha1(print_r($response['auth'], true)), $response['timestamp'], $response['signature'], $reason))){
+			elseif (!($this->Opauth->validate(sha1(print_r($response['auth'], true)), $response['timestamp'], $response['signature'], $reason=''))){
 				$response['error'] = array(
 					'provider' => $response['auth']['provider'],
 					'code' => 'invalid_auth_failed_validation',
